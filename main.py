d40cc50ed1a901d166d9124332335c6932b2238f
Akramovic1 <eng.akramovic@gmail.com>
2021-09-08 21:02:13 +0200

    Turtle is done

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+player = Player()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.listen()
+screen.onkey(player.move, "Up")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
